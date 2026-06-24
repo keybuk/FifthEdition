92e5ab5b0859d60383fb80f1efdba8938380491a
Scott James Remnant <scott@netsplit.com>
2026-06-23 17:49:27 -0700

    Fix errors in CodingTestHelpers
    Trying to catch them with a reported location didn't work out in practice, and Xcode 27 works better reporting the errors thrown by test case without it, so just throw errors again.
    
    Add attachment for recording the encoded JSON in case of failure.

--- Tests/FifthEditionTests/CodingTestHelpers.swift
+++ Tests/FifthEditionTests/CodingTestHelpers.swift
@@ -13,16 +13,16 @@ func testCodable<Value: Equatable & Codable>(json: String,
                                              sourceLocation: SourceLocation = #_sourceLocation)
     throws
 {
-    #expect(throws: Never.self, sourceLocation: sourceLocation) {
-        let value = try JSONDecoder().decode(Value.self, from: json.data(using: .utf8)!)
-        #expect(value == expectedValue, sourceLocation: sourceLocation)
+    let value = try JSONDecoder().decode(Value.self, from: json.data(using: .utf8)!)
+    #expect(value == expectedValue, sourceLocation: sourceLocation)
 
-        // JSONEncoder isn't deterministic when dealing with Set<>, so rather than comparing for exact output, encode
-        // and decode the value, and make sure it stayed the same.
-        let encodedJson = try String(data: JSONEncoder().encode(expectedValue), encoding: .utf8)!
-        let decodedValue = try JSONDecoder().decode(Value.self, from: encodedJson.data(using: .utf8)!)
-        #expect(decodedValue == expectedValue, sourceLocation: sourceLocation)
-    }
+    // JSONEncoder isn't deterministic when dealing with Set<>, so rather than comparing for exact output, encode
+    // and decode the value, and make sure it stayed the same.
+    let encodedJson = try String(data: JSONEncoder().encode(expectedValue), encoding: .utf8)!
+    Attachment.record(encodedJson, named: "Encoded JSON")
+
+    let decodedValue = try JSONDecoder().decode(Value.self, from: encodedJson.data(using: .utf8)!)
+    #expect(decodedValue == expectedValue, sourceLocation: sourceLocation)
 }
 
 func testCodable<Value: Equatable & CodableWithConfiguration>(json: String,
@@ -32,17 +32,17 @@ func testCodable<Value: Equatable & CodableWithConfiguration>(json: String,
     throws
     where Value.DecodingConfiguration == Value.EncodingConfiguration
 {
-    #expect(throws: Never.self, sourceLocation: sourceLocation) {
-        let value = try JSONDecoder().decode(Value.self, from: json.data(using: .utf8)!, configuration: configuration)
-        #expect(value == expectedValue, sourceLocation: sourceLocation)
+    let value = try JSONDecoder().decode(Value.self, from: json.data(using: .utf8)!, configuration: configuration)
+    #expect(value == expectedValue, sourceLocation: sourceLocation)
 
-        // JSONEncoder isn't deterministic when dealing with Set<>, so rather than comparing for exact output, encode
-        // and decode the value, and make sure it stayed the same.
-        let encodedJson = try String(data: JSONEncoder().encode(expectedValue, configuration: configuration),
-                                     encoding: .utf8)!
-        let decodedValue = try JSONDecoder().decode(Value.self,
-                                                    from: encodedJson.data(using: .utf8)!,
-                                                    configuration: configuration)
-        #expect(decodedValue == expectedValue, sourceLocation: sourceLocation)
-    }
+    // JSONEncoder isn't deterministic when dealing with Set<>, so rather than comparing for exact output, encode
+    // and decode the value, and make sure it stayed the same.
+    let encodedJson = try String(data: JSONEncoder().encode(expectedValue, configuration: configuration),
+                                 encoding: .utf8)!
+    Attachment.record(encodedJson, named: "Encoded JSON")
+
+    let decodedValue = try JSONDecoder().decode(Value.self,
+                                                from: encodedJson.data(using: .utf8)!,
+                                                configuration: configuration)
+    #expect(decodedValue == expectedValue, sourceLocation: sourceLocation)
 }
